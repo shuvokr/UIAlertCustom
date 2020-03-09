d9a4e8ab176cadcc63895e89e3bfc9580de662aa
Shuvo Karmakar <shuvo@cmedhealth.com>
2020-03-09 17:14:53 +0600

    Initial commit - setuped the project.

---
+++ UIAlertCustom/UIAlertCustom/UIAlertCustom.h
@@ -0,0 +1,19 @@
+//
+//  UIAlertCustom.h
+//  UIAlertCustom
+//
+//  Created by CMED.Shuvo on 3/9/20.
+//  Copyright © 2020 bigbang.Shuvo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIAlertCustom.
+FOUNDATION_EXPORT double UIAlertCustomVersionNumber;
+
+//! Project version string for UIAlertCustom.
+FOUNDATION_EXPORT const unsigned char UIAlertCustomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIAlertCustom/PublicHeader.h>
+
+
